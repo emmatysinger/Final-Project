d412825e77b06b91d46e006bb982dd6efcf156da
emmatysinger <42844450+emmatysinger@users.noreply.github.com>
2018-10-01 12:11:21 -0400

    Updated from Brython Server: 10/1/2018 12:11:13 PM

--- interactivemap.py
+++ interactivemap.py
@@ -3,9 +3,21 @@
 
 #INTERACTIVE MAP
 from ggame import *
+myapp = App()
 
-#* starfield.jpg: https://www.flickr.com/photos/filterforge/14174682786
-#* US_map.png: https://mbtskoudsalg.com/explore/usa-map-blank-png/#gal_post_3359_america-outline-png-5.png
-MAP = ImageAsset("images/starfield.jpg")
+MAP = ImageAsset("images/US_map.png")
 
 Sprite(MAP)
+
+def capitalquiz:
+    states = {'Alabama':0, 'Alaska':1, 'Arizona':2, 'Arkansas':3, 'California':4, 'Colorado':5, 'Connecticut':6,}
+    capitals = ["Montegomery", "Juneau", 
+    
+def facts:
+
+def findquiz:
+
+myapp.run()
+myapp.ListenKeyEvent('keydown','c',capitalquiz)
+myapp.ListenKeyEvent('keydown','f',findquiz)
+myapp.ListenKeyEvent('click','c',facts)
