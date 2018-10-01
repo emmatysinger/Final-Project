4f9584f49ed63c1abb34834a6dde9b7ecea5f6de
emmatysinger <42844450+emmatysinger@users.noreply.github.com>
2018-10-01 11:54:56 -0400

    Updated from Brython Server: 10/1/2018 11:54:49 AM

--- interactivemap.py
+++ interactivemap.py
@@ -1,1 +1,11 @@
+#Emma Tysinger
+#Credits:
 
+#INTERACTIVE MAP
+from ggame import *
+
+#* starfield.jpg: https://www.flickr.com/photos/filterforge/14174682786
+#* US_map.png: https://mbtskoudsalg.com/explore/usa-map-blank-png/#gal_post_3359_america-outline-png-5.png
+MAP = ImageAsset("images/starfield.jpg")
+
+Sprite(MAP)
