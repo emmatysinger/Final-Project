8acd5aa1ba3db2c5208dd3d2deeb03430ce17a77
emmatysinger <42844450+emmatysinger@users.noreply.github.com>
2018-10-02 08:31:53 -0400

    Updated from Brython Server: 10/2/2018 8:31:49 AM

--- interactivemap.py
+++ interactivemap.py
@@ -3,15 +3,28 @@
 
 #INTERACTIVE MAP
 from ggame import *
+import randint
 myapp = App()
 
 MAP = ImageAsset("images/US_map.png")
 
 Sprite(MAP)
 
+capital = FALSE
+states = {'Alabama':0, 'Alaska':1, 'Arizona':2, 'Arkansas':3, 'California':4, 'Colorado':5, 'Connecticut':6,}
+capitals = ["Montegomery", "Juneau", "Phoenix", "Little Rock", "Sacramento", "Denver", "Hartford"]
+        
 def capitalquiz:
-    states = {'Alabama':0, 'Alaska':1, 'Arizona':2, 'Arkansas':3, 'California':4, 'Colorado':5, 'Connecticut':6,}
-    capitals = ["Montegomery", "Juneau", 
+    global capital, states, capitals
+    capital = not capital
+    while capital = TRUE:
+        n = randint
+        answer = input("What is the capital of" + n + "? ")
+        if answer == capitals[n]:
+            print ("CORRECT!")
+        else:
+            print ("Incorrect, the correct answer is" + capitals[n])
+    
     
 def facts:
 
