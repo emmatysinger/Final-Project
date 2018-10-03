bc2bee5262b2bcc76d9afa09c463a3f731cb1137
emmatysinger <42844450+emmatysinger@users.noreply.github.com>
2018-10-03 12:15:32 -0400

    Updated from Brython Server: 10/3/2018 12:15:27 PM

--- interactivemap.py
+++ interactivemap.py
@@ -7,7 +7,7 @@ import random
 myapp = App()
 
 MAP = ImageAsset("images/united-states-map-png-4-with-transparent-of-usa.png.jpeg")
-Map=Sprite(MAP,(20,20))
+Map=Sprite(MAP,(20,20)
 Map.scale = .44
 coordinates = []
 for s in states:
@@ -19,8 +19,8 @@ for s in states:
 capitalQ = False
 stateQ = False
 states=['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Lousiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
-#states_facts = {'Alabama':("Montegomery"), 'Alaska':("Juneau"), 'Arizona':("Phoenix"), 'Arkansas':("Little Rock"), 'California':("Sacramento"), 'Colorado':( "Denver"), 'Connecticut':("Hartford"), 'Delaware':("Dover"), 'Florida':("Tallahassee"), 'Georgia':("Atlanta"), 'Hawaii':("Honolulu"), 'Idaho':("Boise"), 'Illnois':("Springfield"), 'Indiana':("Indianapolis"), 'Iowa':("Des Moines"), 'Kansas':("Topeka"), 'Kentucky':("Frankfort"), 'Lousiana':("Baton Rouge"), 'Maine':("Augusta"), 'Maryland':("Annapolis"), 'Massachusetts':("Boston"), 'Michigan':("Lansing"), 'Minnesota':("St. Paul"), 'Mississippi':("Jackson"), 'Missouri':("Jefferson City"), 'Montana':("Helena), 'Nebraska':("Lincoln"), 'Nevada':("Carson City"), 'New Hampshire':("Concord"), 'New Jersey':("Trenton"), 'New Mexico':("Santa Fe"), 'New York':("Albany"), 'North Carolina':("Raleigh"), 'North Dakota':("Bismarck"), 'Ohio':("Columbus"), 'Oklahoma':("Oklahoma City"), 'Oregon':("Salem"), 'Pennsylvania':("Harrisburg"), 'Rhode Island':("Providence"), 'South Carolina':("Columbia"), 'South Dakota':("Pierre"), 'Tennessee':("Nashville"), 'Texas':("Austin"), 'Utah':("Salt Lake City"), 'Vermont':("Montpelier"), 'Virginia':("Richmond"), 'Washington':("Olympia"), 'West Virginia':("Charleston"), 'Wisconsin':("Madison"), 'Wyoming':("Cheyenne")}
-#states_location = {'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Lousiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'}
+states_facts = {'Alabama':["Montegomery"], 'Alaska':["Juneau"], 'Arizona':["Phoenix"], 'Arkansas':["Little Rock"], 'California':["Sacramento"], 'Colorado':[ "Denver"], 'Connecticut':["Hartford"], 'Delaware':["Dover"], 'Florida':["Tallahassee"], 'Georgia':["Atlanta"], 'Hawaii':["Honolulu"], 'Idaho':["Boise"], 'Illnois':["Springfield"], 'Indiana':["Indianapolis"], 'Iowa':["Des Moines"], 'Kansas':["Topeka"], 'Kentucky':["Frankfort"], 'Lousiana':["Baton Rouge"], 'Maine':["Augusta"], 'Maryland':["Annapolis"], 'Massachusetts':["Boston"], 'Michigan':["Lansing"], 'Minnesota':["St. Paul"], 'Mississippi':["Jackson"], 'Missouri':["Jefferson City"], 'Montana':["Helena], 'Nebraska':["Lincoln"], 'Nevada':["Carson City"], 'New Hampshire':["Concord"], 'New Jersey':["Trenton"], 'New Mexico':["Santa Fe"], 'New York':["Albany"], 'North Carolina':["Raleigh"], 'North Dakota':["Bismarck"], 'Ohio':["Columbus"], 'Oklahoma':["Oklahoma City"], 'Oregon':["Salem"], 'Pennsylvania':["Harrisburg"], 'Rhode Island':["Providence"], 'South Carolina':["Columbia"], 'South Dakota':["Pierre"], 'Tennessee':["Nashville"], 'Texas':["Austin"], 'Utah':["Salt Lake City"], 'Vermont':["Montpelier"], 'Virginia':["Richmond"], 'Washington':["Olympia"], 'West Virginia':["Charleston"], 'Wisconsin':["Madison"], 'Wyoming':["Cheyenne"]}
+states_location = {'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Lousiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'}
  
         
 def capitalquiz(event):
@@ -32,10 +32,10 @@ def capitalquiz(event):
         if answer == states_facts[state]:
             print ("CORRECT!")
         else:
-            print ("Incorrect, the correct answer is" + capitals[n])
+            print ("Incorrect, the correct answer is" + capitals[n]]
     
 
-def findquiz(event):
+def findquiz(event]:
     global stateQ, states, states_location
     stateQ = not stateQ
     if state == True:
