746ec669ccb05abb1b1b0026441dc832bb508eba
emmatysinger <42844450+emmatysinger@users.noreply.github.com>
2018-10-03 11:41:16 -0400

    Updated from Brython Server: 10/3/2018 11:41:10 AM

--- interactivemap.py
+++ interactivemap.py
@@ -6,33 +6,47 @@ from ggame import *
 import random
 myapp = App()
 
-MAP = ImageAsset("images/US_map.png")
+MAP = ImageAsset("images/united-states-map-png-4-with-transparent-of-usa.png.jpeg")
+Map=Sprite(MAP,(20,20))
+Map.scale = .44
+coordinates = []
+for s in states:
+    xy = states_location[s]
+    coordinates.append(xy)
 
-Sprite(MAP)
 
-capital = False
-states=['Alabama':("Montegomery"), 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Lousiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
+#---------------------------------------------------------------------------------------------------------------
+capitalQ = False
+stateQ = False
+states=['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Lousiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
 states_facts = {'Alabama':("Montegomery"), 'Alaska':("Juneau"), 'Arizona':("Phoenix"), 'Arkansas':("Little Rock"), 'California':("Sacramento"), 'Colorado':( "Denver"), 'Connecticut':("Hartford"), 'Delaware':("Dover"), 'Florida':("Tallahassee"), 'Georgia':("Atlanta"), 'Hawaii':("Honolulu"), 'Idaho':("Boise"), 'Illnois':("Springfield"), 'Indiana':("Indianapolis"), 'Iowa':("Des Moines"), 'Kansas':("Topeka"), 'Kentucky':("Frankfort"), 'Lousiana':("Baton Rouge"), 'Maine':("Augusta"), 'Maryland':("Annapolis"), 'Massachusetts':("Boston"), 'Michigan':("Lansing"), 'Minnesota':("St. Paul"), 'Mississippi':("Jackson"), 'Missouri':("Jefferson City"), 'Montana':("Helena), 'Nebraska':("Lincoln"), 'Nevada':("Carson City"), 'New Hampshire':("Concord"), 'New Jersey':("Trenton"), 'New Mexico':("Santa Fe"), 'New York':("Albany"), 'North Carolina':("Raleigh"), 'North Dakota':("Bismarck"), 'Ohio':("Columbus"), 'Oklahoma':("Oklahoma City"), 'Oregon':("Salem"), 'Pennsylvania':("Harrisburg"), 'Rhode Island':("Providence"), 'South Carolina':("Columbia"), 'South Dakota':("Pierre"), 'Tennessee':("Nashville"), 'Texas':("Austin"), 'Utah':("Salt Lake City"), 'Vermont':("Montpelier"), 'Virginia':("Richmond"), 'Washington':("Olympia"), 'West Virginia':("Charleston"), 'Wisconsin':("Madison"), 'Wyoming':("Cheyenne")}
+states_location = {'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Lousiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'}
  
         
 def capitalquiz(event):
-    global capital, states, states_facts
-    capital = not capital
+    global capitalQ, states, states_facts
+    capitalQ = not capitalQ
     if capital == True:
-        n = random.randint(0, 50)
-        state = states[n]
+        state = random.choice(states)
         answer = input("What is the capital of" + state + "? ")
         if answer == states_facts[state]:
             print ("CORRECT!")
         else:
             print ("Incorrect, the correct answer is" + capitals[n])
     
+
+def findquiz(event):
+    global stateQ, states, states_location
+    stateQ = not stateQ
+    if state == True:
+        state = random.choice(states)
+
+def facts(event):
+    global stateQ, capitalQ, states_location, states_facts
+    #while stateQ == False and capitalQ == False:
+
     
-# def facts(event):
-
-# def findquiz(event):
-
 myapp.run()
 myapp.listenKeyEvent('keydown','c', capitalquiz)
-# myapp.listenKeyEvent('keydown','f',findquiz)
-# myapp.listenKeyEvent('click','c',facts)
+myapp.listenKeyEvent('keydown','f',findquiz)
+myapp.listenKeyEvent('click','c',facts)
