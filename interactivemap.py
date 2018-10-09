e9c5dcf30fd4bcb776224d09fc686a34ddc467c7
emmatysinger <42844450+emmatysinger@users.noreply.github.com>
2018-10-09 11:24:06 -0400

    Updated from Brython Server: 10/9/2018 11:24:01 AM

--- interactivemap.py
+++ interactivemap.py
@@ -20,21 +20,23 @@ states=['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'C
 states_facts = {'Alabama':["Montegomery"], 'Alaska':["Juneau"], 'Arizona':["Phoenix"], 'Arkansas':["Little Rock"], 'California':["Sacramento"], 'Colorado':[ "Denver"], 'Connecticut':["Hartford"], 'Delaware':["Dover"], 'Florida':["Tallahassee"], 'Georgia':["Atlanta"], 'Hawaii':["Honolulu"], 'Idaho':["Boise"], 'Illnois':["Springfield"], 'Indiana':["Indianapolis"], 'Iowa':["Des Moines"], 'Kansas':["Topeka"], 'Kentucky':["Frankfort"], 'Lousiana':["Baton Rouge"], 'Maine':["Augusta"], 'Maryland':["Annapolis"], 'Massachusetts':["Boston"], 'Michigan':["Lansing"], 'Minnesota':["St. Paul"], 'Mississippi':["Jackson"], 'Missouri':["Jefferson City"], 'Montana':["Helena"], 'Nebraska':["Lincoln"], 'Nevada':["Carson City"], 'New Hampshire':["Concord"], 'New Jersey':["Trenton"], 'New Mexico':["Santa Fe"], 'New York':["Albany"], 'North Carolina':["Raleigh"], 'North Dakota':["Bismarck"], 'Ohio':["Columbus"], 'Oklahoma':["Oklahoma City"], 'Oregon':["Salem"], 'Pennsylvania':["Harrisburg"], 'Rhode Island':["Providence"], 'South Carolina':["Columbia"], 'South Dakota':["Pierre"], 'Tennessee':["Nashville"], 'Texas':["Austin"], 'Utah':["Salt Lake City"], 'Vermont':["Montpelier"], 'Virginia':["Richmond"], 'Washington':["Olympia"], 'West Virginia':["Charleston"], 'Wisconsin':["Madison"], 'Wyoming':["Cheyenne"]}
 states_location = {'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Lousiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'}
 
-for s in states:
-    xy = states_location[s]
-    coordinates.append(xy)
+# for s in states:
+#     xy = states_location[s]
+#     coordinates.append(xy)
 
      
 def capitalquiz(event):
     global capitalQ, states, states_facts
     capitalQ = not capitalQ
-    if capital == True:
+    if capitalQ == True:
         state = random.choice(states)
-        answer = input("What is the capital of" + state + "? ")
-        if answer == states_facts[state]:
+        answer = input("What is the capital of " + state + "? ")
+        correct_answer = states_facts[state]
+        correct_answer = correct_answer[0]
+        if answer == correct_answer:
             print ("CORRECT!")
         else:
-            print ("Incorrect, the correct answer is" + capitals[n])
+            print ("Incorrect, the correct answer is " + correct_answer)
     
 
 def findquiz(event):
