7d27070f516fccb8782e61a7bd90b8f1e2afb551
emmatysinger <42844450+emmatysinger@users.noreply.github.com>
2018-10-09 23:18:27 -0400

    Updated from Brython Server: 10/9/2018 11:18:27 PM

--- interactivemap.py
+++ interactivemap.py
@@ -6,6 +6,7 @@ from ggame import *
 import random
 import time
 import browser
+import threading
 myapp = App()
 
 MAP = ImageAsset("images/united-states-map-png-4-with-transparent-of-usa.png.jpeg")
@@ -28,6 +29,7 @@ for s in coordinates:
 capitalQ = False
 stateQ = False
 ready = False
+go = Event()
 states=['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Lousiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
 states_facts = {'Alabama':["Montegomery"], 'Alaska':["Juneau"], 'Arizona':["Phoenix"], 'Arkansas':["Little Rock"], 'California':["Sacramento"], 'Colorado':[ "Denver"], 'Connecticut':["Hartford"], 'Delaware':["Dover"], 'Florida':["Tallahassee"], 'Georgia':["Atlanta"], 'Hawaii':["Honolulu"], 'Idaho':["Boise"], 'Illnois':["Springfield"], 'Indiana':["Indianapolis"], 'Iowa':["Des Moines"], 'Kansas':["Topeka"], 'Kentucky':["Frankfort"], 'Lousiana':["Baton Rouge"], 'Maine':["Augusta"], 'Maryland':["Annapolis"], 'Massachusetts':["Boston"], 'Michigan':["Lansing"], 'Minnesota':["St. Paul"], 'Mississippi':["Jackson"], 'Missouri':["Jefferson City"], 'Montana':["Helena"], 'Nebraska':["Lincoln"], 'Nevada':["Carson City"], 'New Hampshire':["Concord"], 'New Jersey':["Trenton"], 'New Mexico':["Santa Fe"], 'New York':["Albany"], 'North Carolina':["Raleigh"], 'North Dakota':["Bismarck"], 'Ohio':["Columbus"], 'Oklahoma':["Oklahoma City"], 'Oregon':["Salem"], 'Pennsylvania':["Harrisburg"], 'Rhode Island':["Providence"], 'South Carolina':["Columbia"], 'South Dakota':["Pierre"], 'Tennessee':["Nashville"], 'Texas':["Austin"], 'Utah':["Salt Lake City"], 'Vermont':["Montpelier"], 'Virginia':["Richmond"], 'Washington':["Olympia"], 'West Virginia':["Charleston"], 'Wisconsin':["Madison"], 'Wyoming':["Cheyenne"]}
 
@@ -58,14 +60,15 @@ def capitalquiz(event):
             print ("Incorrect, the capital of {0} is {1}!".format(state, correct_answer))
     
 
-def findquiz(event):
-    global stateQ, states, state, ready
+def findquiz(event, go=None):
+    global stateQ, states, state, ready, go
     stateQ = not stateQ
     ready = False
     if stateQ == True:
         r_state = random.choice(states)
-        while not ready:
-            browser.timer(1)
+        go.wait()
+        # while not ready:
+        #     browser.timer(1)
         if r_state == state:
             print("CORRECT!")
         else:
@@ -73,7 +76,7 @@ def findquiz(event):
         
 
 def facts(event):
-    global stateQ, capitalQ, states_facts, determinestate, ready
+    global stateQ, capitalQ, states_facts, determinestate, ready, go
     ready = True
     if stateQ == False and capitalQ == False:
         determinestate(event.x,event.y)
@@ -89,6 +92,8 @@ Population:
     
     elif stateQ == True:
         determinestate(event.x,event.y)
+        go = go
+
 
     
 myapp.run()
