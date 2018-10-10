0efe792443d74e5fe2996bb004e725333a9ea812
emmatysinger <42844450+emmatysinger@users.noreply.github.com>
2018-10-09 23:45:57 -0400

    Updated from Brython Server: 10/9/2018 11:45:57 PM

--- interactivemap.py
+++ interactivemap.py
@@ -21,6 +21,7 @@ line = LineStyle(1, black)
 dot = CircleAsset(8, line, white)
 state_int = -1
 state = 0
+r_state = 0
 
 for s in coordinates:
     x,y = s
@@ -30,7 +31,6 @@ for s in coordinates:
 capitalQ = False
 stateQ = False
 ready = False
-go = threading.Event()
 
 states=['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Lousiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
 states_facts = {'Alabama':["Montegomery"], 'Alaska':["Juneau"], 'Arizona':["Phoenix"], 'Arkansas':["Little Rock"], 'California':["Sacramento"], 'Colorado':[ "Denver"], 'Connecticut':["Hartford"], 'Delaware':["Dover"], 'Florida':["Tallahassee"], 'Georgia':["Atlanta"], 'Hawaii':["Honolulu"], 'Idaho':["Boise"], 'Illnois':["Springfield"], 'Indiana':["Indianapolis"], 'Iowa':["Des Moines"], 'Kansas':["Topeka"], 'Kentucky':["Frankfort"], 'Lousiana':["Baton Rouge"], 'Maine':["Augusta"], 'Maryland':["Annapolis"], 'Massachusetts':["Boston"], 'Michigan':["Lansing"], 'Minnesota':["St. Paul"], 'Mississippi':["Jackson"], 'Missouri':["Jefferson City"], 'Montana':["Helena"], 'Nebraska':["Lincoln"], 'Nevada':["Carson City"], 'New Hampshire':["Concord"], 'New Jersey':["Trenton"], 'New Mexico':["Santa Fe"], 'New York':["Albany"], 'North Carolina':["Raleigh"], 'North Dakota':["Bismarck"], 'Ohio':["Columbus"], 'Oklahoma':["Oklahoma City"], 'Oregon':["Salem"], 'Pennsylvania':["Harrisburg"], 'Rhode Island':["Providence"], 'South Carolina':["Columbia"], 'South Dakota':["Pierre"], 'Tennessee':["Nashville"], 'Texas':["Austin"], 'Utah':["Salt Lake City"], 'Vermont':["Montpelier"], 'Virginia':["Richmond"], 'Washington':["Olympia"], 'West Virginia':["Charleston"], 'Wisconsin':["Madison"], 'Wyoming':["Cheyenne"]}
@@ -63,7 +63,7 @@ def capitalquiz(event):
     
 
 def findquiz(event):
-    global stateQ, states, state, ready, go
+    global stateQ, states, state, ready, go, r_state
     stateQ = not stateQ
     ready = False
     if stateQ == True:
@@ -71,15 +71,10 @@ def findquiz(event):
         print("Where is {0}?".format(r_state))
         # while not ready:
         #     browser.timer(1)
-        go.wait()
-        if r_state == state:
-            print("CORRECT!")
-        else:
-            print("Incorrect")
         
 
 def facts(event):
-    global stateQ, capitalQ, states_facts, determinestate, ready, go
+    global stateQ, capitalQ, states_facts, determinestate, ready, r_state
     ready = True
     if stateQ == False and capitalQ == False:
         determinestate(event.x,event.y)
@@ -95,7 +90,10 @@ Population:
     
     elif stateQ == True:
         determinestate(event.x,event.y)
-        go.set()
+        if r_state == state:
+            print("CORRECT!")
+        else:
+            print("Incorrect")
 
 
     
