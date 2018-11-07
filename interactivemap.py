ab25413d89ec255183c720f0f4c8e12ab8c7da3f
emmatysinger <42844450+emmatysinger@users.noreply.github.com>
2018-11-07 12:04:03 -0500

    Updated from Brython Server: 11/7/2018 12:04:01 PM

--- interactivemap.py
+++ interactivemap.py
@@ -64,8 +64,8 @@ class FACT(Sprite):
         super().__init__(fact, (0.75*width, 0.8*height))
     
     def invisible(self):
-        self.BOX.destroy()
-        self.destroy()
+        self.BOX.destroy = False
+        self.visible = False
         
 class Correct(Sprite):
     global go
