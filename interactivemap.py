70f8b91e2126b11ed7f3a584fc2ca9c7712f6be9
emmatysinger <42844450+emmatysinger@users.noreply.github.com>
2018-11-20 11:40:21 -0500

    Updated from Brython Server: 11/20/2018 11:40:20 AM

--- interactivemap.py
+++ interactivemap.py
@@ -8,6 +8,15 @@ import random
 
 myapp = App()
 
+#-----------    SPRITE INFO ----------------------------------------------------------------------------#
+white = Color(0xfffafa, 1)
+black = Color(0x000000, 1)
+Medium_Aquamarine = Color(0x66cdaa, 1)
+Pink = Color(0xeea2ad, 1)
+line = LineStyle(1, black)
+no_line = LineStyle(1, white)
+
+#-----------    CREATE MAP ----------------------------------------------------------------------------#
 MAP = ImageAsset("images/united-states-map-png-4-with-transparent-of-usa.png.jpeg")
 Map=Sprite(MAP,(20,20))
 
@@ -20,20 +29,25 @@ if width/mp_w < 0.8*height/mp_h:
 else:
     Map.scale = (0.8*height-20)/mp_h
 
+#-----------    STATE COORDINATES   --------------------------------------------------------------------------#
 coordinates = []
 coordinates_i = [(740,480), (135,560), (225,415), (610,425), (85,335), (360,320), (965,200), (990,290), (865,573), (805,465), (370,645), (210,170), (670,295), (725,290), (585,245), (495,335), (765,335), (610,505), (1010,100), (905,280), (985,180), (740,205), (565,135), (675,475), (610,340), (310,105), (470,255), (145,260), (975,155), (940,260), (335,425), (910,175), (890,375), (465,110), (790,275), (515,405), (100,145), (875,240), (1015,240), (850,420), (460,180), (740,390), (475,500), (240,295), (950,135), (885,320), (130,60), (825,315), (645,170), (330,205)] 
 for s in coordinates_i: 
     x,y = s
     coordinates.append((x/.44*Map.scale,y/.44*Map.scale))
 
-white = Color(0xfffafa, 1)
-black = Color(0x000000, 1)
-Medium_Aquamarine = Color(0x66cdaa, 1)
-Pink = Color(0xeea2ad, 1)
-line = LineStyle(1, black)
-no_line = LineStyle(1, white)
 dot_size = 8/.44*Map.scale
 dot = CircleAsset(dot_size, line, white)
+for s in coordinates:
+    x,y = s
+    Sprite(dot, (x, y))
+    
+Delaware = LineAsset(57/0.44*Map.scale, 4/.44*Map.scale, line)
+Delaware = Sprite(Delaware, (935/0.44*Map.scale,290/0.44*Map.scale))
+RI = LineAsset(25/0.44*Map.scale, 35/.44*Map.scale, line)
+RI = Sprite(RI, (1000/0.44*Map.scale,205/0.44*Map.scale))
+
+#-----------    VARIABLES   --------------------------------------------------------------------------#
 blank = CircleAsset(1, no_line, white)
 state_int = -1
 state = 0
@@ -43,16 +57,7 @@ time = Sprite(blank, (width,height))
 box = Sprite(blank, (width,height))
 go = 0
 
-for s in coordinates:
-    x,y = s
-    Sprite(dot, (x, y))
-
-Delaware = LineAsset(57/0.44*Map.scale, 4/.44*Map.scale, line)
-Delaware = Sprite(Delaware, (935/0.44*Map.scale,290/0.44*Map.scale))
-RI = LineAsset(25/0.44*Map.scale, 35/.44*Map.scale, line)
-RI = Sprite(RI, (1000/0.44*Map.scale,205/0.44*Map.scale))
-
-#---------------------------------------------------------------------------------------------------------------
+#------------    SPRITE CLASSES    --------------------------------------------------------------------------#
 class Counter(Sprite):
     def __init__(self, count):
         texto = TextAsset(text = str(count), style = '45px Helvetica', color = black)
@@ -93,6 +98,7 @@ class Instructions(Sprite):
         instruction = TextAsset(text = instruct, style ='18px Helvetica', width = wid)
         super().__init__(instruction, position)
 
+#------------    INSTRUCTIONS    --------------------------------------------------------------------------#
 Gbox = RectangleAsset(350,0.08*height, line, Pink)
 Gbox = Sprite(Gbox, (0,0.919*height))
 Fbox = RectangleAsset(275,0.18*height, line, Pink)
@@ -117,18 +123,22 @@ C.visible = False
 Fbox.visible = False
 Cbox.visible = False
     
-#---------------------------------------------------------------------------------------------------------------
+#----------    MORE VARIABLES   -------------------------------------------------------------------------#
 i = FACT(" ", no_line, white)
 yay = Correct()
 yay.invisible()
-#---------------------------------------------------------------------------------------------------------------
 capitalQ = False
 stateQ = False
 Time = False
 
+#----------   STATE DATA    -----------------------------------------------------------------------------#
 states=['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Lousiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
 states_facts = {'Alabama':["Montegomery", "4,888,000", "Yellowhammer"], 'Alaska':["Juneau", "740,000", "Last Frontier"], 'Arizona':["Phoenix", "7,123,000", "Grand Canyon"], 'Arkansas':["Little Rock", "3,020,000", "Natural"], 'California':["Sacramento", "39,776,000", "Golden"], 'Colorado':["Denver", "5,684,000", "Centennial"], 'Connecticut':["Hartford", "3,588,000", "Constitution"], 'Delaware':["Dover", "971,180", "First"], 'Florida':["Tallahassee", "21,312,000", "Sunshine"], 'Georgia':["Atlanta", "10,545,000", "Peach"], 'Hawaii':["Honolulu", "1,426,000", "Aloha"], 'Idaho':["Boise", "1,753,000", "Gem"], 'Illinois':["Springfield", "12,768,000", "Prairie"], 'Indiana':["Indianapolis", "6,699,000", "Hoosier"], 'Iowa':["Des Moines", "3,160,000", "Hawkeye"], 'Kansas':["Topeka", "2,918,000", "Sunflower"], 'Kentucky':["Frankfort", "4,472,000", "Bluegrass"], 'Lousiana':["Baton Rouge", "4,682,000", "Pelican"], 'Maine':["Augusta", "1,341,000", "Pine Tree"], 'Maryland':["Annapolis", "6,079,000", "Old Line"], 'Massachusetts':["Boston", "6,895,000", "Bay"], 'Michigan':["Lansing", "9,991,000", "Great Lakes"], 'Minnesota':["St. Paul", "5,628,000", "North Star"], 'Mississippi':["Jackson", "2,982,000", "Magnolia"], 'Missouri':["Jefferson City", "6,135,000", "Show Me"], 'Montana':["Helena", "1,062,000", "Treasure"], 'Nebraska':["Lincoln", "1,932,000", "Cornhusker"], 'Nevada':["Carson City", "3,056,000", "Silver"], 'New Hampshire':["Concord", "1,350,000", "Granite"], 'New Jersey':["Trenton", "9,032,000", "Garden"], 'New Mexico':["Santa Fe", "2,090,000", "Land of Enchantment"], 'New York':["Albany", "19,862,000", "Empire"], 'North Carolina':["Raleigh", "10,390,000", "Tar Heel"], 'North Dakota':["Bismarck", "755,000", "Peace Garden"], 'Ohio':["Columbus", "11,694,000", "Buckeye"], 'Oklahoma':["Oklahoma City", "3,940,000", "Sooner"], 'Oregon':["Salem", "4,199,593", "Beaver"], 'Pennsylvania':["Harrisburg", "12,823,000", "Keystone"], 'Rhode Island':["Providence", "1,061,000", "Ocean"], 'South Carolina':["Columbia", "5,088,000", "Palmetto"], 'South Dakota':["Pierre", "877,790", "Mount Rushmore"], 'Tennessee':["Nashville", "6,782,000", "Volunteer"], 'Texas':["Austin", "28,704,330", "Lone Star"], 'Utah':["Salt Lake City", "3,159,000", "Beehive"], 'Vermont':["Montpelier", "623,000", "Green Mountain"], 'Virginia':["Richmond", "8,525,000", "Old Dominion"], 'Washington':["Olympia", "7,530,000", "Evergreen"], 'West Virginia':["Charleston", "1,803,000", "Mountain"], 'Wisconsin':["Madison", "5,818,000", "Badger"], 'Wyoming':["Cheyenne", "573,000", "Equality"]}
-#---------------------------------------------------------------------------------------------------------------
+
+#----------   UNIVERSAL FUNCTIONS    ---------------------------------------------------------------------#
+def rand_state():
+    global state
+
 def determinestate(x,y):
     global state_int, coordinates, state, states
     for s in range(len(coordinates)):
@@ -230,7 +240,7 @@ def step():
         else:
             o += 1
 
-#---------------------------------------------------------------------------------------------------------------
+#--------   KEY EVENTS    ------------------------------------------------------------------------------#
 def capitalquiz(event):
     global capitalQ, i
     capitalQ = not capitalQ
